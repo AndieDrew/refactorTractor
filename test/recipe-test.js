86a00b3943008a19cdec50c4b5591a224697be31
Patrick Findley <patfindley@gmail.com>
2021-04-13 22:07:58 -0400

    Add new recipe instantiation

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -4,29 +4,39 @@ import Recipe from '../src/recipe.js';
 import recipeData from '../src/data/recipes.js';
 import ingredientsData from '../src/data/ingredients.js';
 
-let recipe;
+let recipe, recipe1;
 
 describe('Recipe', () => {
   beforeEach(() => {
 
-    recipe = new Recipe(recipeData[47], ingredientsData);
+    recipe = new Recipe(recipeData[0], ingredientsData);
+    recipe1 = new Recipe(recipeData[1], ingredientsData);
   });
 
   describe('Recipe Data', () => {
 
+    it('Should hold recipe name', () => {
+      expect(recipe.name).to.equal("Loaded Chocolate Chip Pudding Cookie Cups");
+      expect(recipe1.name).to.equal("Maple Dijon Apple Cider Grilled Pork Chops");
+    })
+
     it('Should hold its own ingredient data', () => {
-      expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
+      expect(recipe.ingredients).to.equal(recipeData[0].ingredients);
     })
 
-    it('Should hold its own instruction data', () => {
-      expect(recipe.instructions).to.equal(recipeData[47].instructions);
+    it.only('Should hold its own instruction data', () => {
+      expect(recipe.instructions).to.equal(recipeData[0].instructions);
     })
+
+    it('Should hold tags for its own recipe', () => {
+      expect(recipe.tags).to.deep.equal(["antipasti", "starter", "snack", "appetizer", "antipasto", "hor d'oeuvre"]);
+    })
+
+    it('Should be able to calculate the cost of its ingredients', () => {
+      console.log(ingredientsData);
+      expect(recipe.calculateCost()).to.equal(4166);
+    });
+
+
   })
-
-  it('Should be able to calculate the cost of its ingredients', () => {
-    // console.log(ingredientsData);
-    expect(recipe.calculateCost()).to.equal(4166);
-  });
-
-
 });
