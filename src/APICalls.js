c7133e57edd25081c256a7a50ab03e39c4839f39
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-04-13 21:05:06 -0400

    Fetch recipe data from local API

--- src/APICalls.js
+++ src/APICalls.js
@@ -2,22 +2,30 @@ export const fetchData = () => {
   let userData = fetch("http://localhost:3001/api/v1/users")
     .then(response => response.json())
     .then(userData => {
-      return userData
+      return userData;
     })
-    .catch(err => console.log("error received"))
+    .catch(err => console.log("error received"));
 
   let ingredientsData = fetch("http://localhost:3001/api/v1/ingredients")
     .then(response => response.json())
     .then(ingredientsData => {
-      return ingredientsData
+      return ingredientsData;
     })
-    .catch(err => console.log("error received"))
+    .catch(err => console.log("error received"));
 
-    return Promise.all([userData, ingredientsData])
+    let recipeData = fetch("http://localhost:3001/api/v1/recipes")
+      .then(response => response.json())
+      .then(recipeData => {
+        return recipeData;
+      })
+      .catch(err => console.log("recipeData error received"));
+
+    return Promise.all([userData, ingredientsData, recipeData])
     .then(data => {
       let allData = {};
-      allData.userData = data[0]
+      allData.userData = data[0];
       allData.ingredientsData = data[1];
+      allData.recipeData = data[2];
       return allData;
     })
 }
