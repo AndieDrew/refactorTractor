433899a73652271203011ef6bc3180a56fa263c7
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-04-13 21:21:46 -0600

    Add pantry test data

---
+++ src/data/test-users.js
@@ -0,0 +1,28 @@
+let users = [
+  {
+    "id": 1,
+    "name": "Mr. Has Ingredients",
+    "pantry": [
+      {
+        "ingredient": 11477,
+        "amount": 1
+      },
+      {
+        "ingredient": 93820,
+        "amount": 1
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Mr. Has Nada",
+    "pantry": [
+      {
+        "ingredient": 420,
+        "amount": 69
+      }
+    ]
+  }
+]
+
+export default users;
--- test/pantry-test.js
+++ test/pantry-test.js
@@ -5,20 +5,20 @@ import Recipe from '../src/recipe.js';
 import Pantry from '../src/pantry.js';
 
 import recipeData from '../src/data/recipes.js'
-import userData from '../src/data/recipes.js'
+import users from '../src/data/test-users.js'
 
 
 let user, pantry;
 
 describe('Pantry', () => {
   beforeEach(() => {
-    let currentUser = userData.users[0]
+    let currentUser = users[0]
     user = new User(1, currentUser.name, currentUser.pantry)
     pantry = new Pantry(currentUser.pantry)
   });
 
   it('Should be able to check all ingredients existing inside pantry', () => {
-    expect(pantry.takeInventory()).to.deep.eql(ingredients);
+    expect(pantry.takeInventory()).to.deep.eql(user.pantry);
   });
 
   it('Should be able to check ingredients in User\'s pantry for a given recipe', () => {
@@ -26,7 +26,7 @@ describe('Pantry', () => {
   });
 
   it('Should inform User if they lack required ingredients for a given recipe', () => {
-    expect(pantry.checkPantry(recipeIngredients)).to.eql(missingIngredientsWithPrice);
+    expect(pantry.checkPantry(recipeData[1])).to.eql(missingIngredientsWithPrice);
   });
 
   it('Should remove ingredients from pantry if User is able to use them to cook a recipe', () => {
