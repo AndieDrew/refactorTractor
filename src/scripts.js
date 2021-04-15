a1f9d6cb1f270b03dec89c07777555a9bd552ae0
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-04-14 22:07:22 -0400

    Add function that populates cardArea with search results

--- src/scripts.js
+++ src/scripts.js
@@ -48,6 +48,29 @@ function fetchCurrentData() {
   })
 }
 
+function inputSearch() {
+  console.log(cookbook.recipies)
+    cardArea.innerHTML = " ";
+    cookbook.findRecipe(searchInput.value.toLowerCase()).forEach(recipe => {
+      cardArea.insertAdjacentHTML('afterbegin', `<div id='${recipe.id}'
+      class='card'>
+      <header id='${recipe.id}' class='card-header'>
+      <label for='add-button' class='hidden'>Click to add recipe</label>
+      <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
+      <img id='${recipe.id}' class='add'
+      src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
+      recipes to cook'></button>
+      <label for='favorite-button' class='hidden'>Click to favorite recipe
+      </label>
+      <button id='${recipe.id}' aria-label='favorite-button' class='favorite favorite${recipe.id} card-button'>
+      </button></header>
+      <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+      <img id='${recipe.id}' tabindex='0' class='card-picture'
+      src='${recipe.image}' alt='Food from recipe'>
+      </div>`)
+    })
+}
+
 function viewFavorites() {
   if (cardArea.classList.contains('all')) {
     cardArea.classList.remove('all')
